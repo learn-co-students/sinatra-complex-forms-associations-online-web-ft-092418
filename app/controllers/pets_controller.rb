d593ef700453216a5d79c9d539139b11edcb55dc
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-11-12 16:16:45 -0500

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,25 +2,53 @@ class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index' 
+    erb :'/pets/index'
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
-  post '/pets' do 
-
+  post '/pets' do
+   #binding.pry
+    @pet = Pet.create(params[:pet])
+  #  binding.pry
+    if !params["pet"]["owner_id"]
+    #  binding.pry
+      @pet.owner = Owner.create(name: params["owner_name"])
+      @pet.save
+    end
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do 
+  get "/pets/:id/edit" do
+    @pet = Pet.find_by_id(params[:id])
+    @owners = Owner.all
+    #binding.pry
+    erb :"/pets/edit"
+  end
 
+  patch '/pets/:id' do
+    # if !params[:pet].keys.include?("owner_id")
+    #   params[:pet]["owner_id"] = []
+    # end
+    #######
+    #binding.pry
+    @pet = Pet.find(params[:id])
+    @pet.update(name: params[:pet_name], owner_id: params[:pet][:owner_id])
+
+  # binding.pry
+    if params["owner"]["name"] != ""
+      #binding.pry
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+    end
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 end
